9f56af232d872a77c77d14e01d197fede974817f
ajomuch92 <aaronmontes_92@yahoo.es>
2022-06-19 15:09:36 -0600

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { V3BulmaSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  V3BulmaSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v3-bulma-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as V3BulmaSample } from './v3-bulma-sample.vue';
