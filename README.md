5ff053ea0f9d3d4b9bdbcb15f2d0f7d1a0d9652e
ajomuch92 <aaronmontes_92@yahoo.es>
2022-09-08 20:00:52 -0600

    Configuring Vite files to publish package

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Vue 3 + Vite
+# V3 Bulma
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+This is an Open Source project. A library of components for Vue 3 with Bulma
 
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+WORK IN PROGRESS
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'v3-bulma',
+      fileName: (format) => `v3-bulma.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()]
 })
