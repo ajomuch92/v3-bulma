70721e123bd1fe5eda2c4fc3293060d99469132c
ajomuch92 <aaronmontes_92@yahoo.es>
2022-09-11 13:15:25 -0600

    feat: 🎸 Creating V3Tag component

---
+++ src/components/V3Tag.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="tag" :class="innerClass">
+    <slot />
+  </span>
+</template>
+
+<script>
+import { size } from '../utils/sizes';
+import { color } from '../utils/colors';
+
+export default {
+  name: 'V3Tag',
+  props: {
+    size,
+    color,
+    light: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const innerClass = computed(() => {
+      const classes = [];
+      if (props.color) classes.push(`is-${props.color}`);
+      if (props.size) classes.push(`is-${props.size}`);
+      if (props.light) classes.push('is-light');
+      return classes;
+    });
+
+    return {
+      innerClass,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
