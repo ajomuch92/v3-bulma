3b6b60173ed1f477d299cb6f7cd81c2c1a7ca3a3
ajomuch92 <aaronmontes_92@yahoo.es>
2022-09-11 11:10:38 -0600

    feat: 🎸 Creating components related with icons

---
+++ src/components/V3Icon.vue
@@ -0,0 +1,48 @@
+<template>
+  <span class="icon">
+    <i :class="`${pack}-${type} fa-${iconName} ${innerClass}`"></i>
+  </span>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+import { size } from '../utils/sizes';
+import { color } from '../utils/colors';
+
+export default {
+  name: 'V3Icon',
+  props: {
+    iconName: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: (val) => ['regular', 'solid'].includes(val),
+    },
+    pack: {
+      type: String,
+      default: 'fa'
+    },
+    size,
+    color,
+  },
+  setup(props) {
+    const innerClass = computed(() => {
+      let classIn = '';
+      if (props.size) classIn += ` is-${props.size} `;
+      if (props.color) classIn += ` is-${props.color} `;
+      return classIn.trim();
+    });
+
+    return {
+      innerClass,
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/V3IconText.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="icon-text">
+    <slot name="icon" />
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'V3IconText'
+}
+</script>
+
+<style>
+
+</style>
