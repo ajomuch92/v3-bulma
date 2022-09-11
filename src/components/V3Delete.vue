e2a565e547cdfc23ac833e5252c3a51d27b5bdc6
ajomuch92 <aaronmontes_92@yahoo.es>
2022-09-11 11:10:14 -0600

    feat: 🎸 Creating V3Delete button component

---
+++ src/components/V3Delete.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="delete" :class="innerClass" @click="clickHandler" />
+</template>
+
+<script>
+import { size } from '../utils/sizes';
+
+export default {
+  name: 'V3Delete',
+  props: {
+    size,
+  },
+  setup(props, { emit }) {
+    const clickHandler = (e) => emit('click', e);
+
+    const innerClass = computed(() => {
+      const classes = [];
+      if (props.size) classes.push(`is-${props.size}`);
+      return classes;
+    });
+
+    return {
+      innerClass,
+      clickHandler,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
