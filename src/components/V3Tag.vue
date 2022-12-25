e5bfd80201a4fea7c65d59e0395b91b0bca40bc7
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-25 16:53:52 -0600

    refactor: 💡 Updating tag component to add delete button

--- src/components/V3Tag.vue
+++ src/components/V3Tag.vue
@@ -1,16 +1,19 @@
 <template>
   <span class="tag" :class="innerClass">
     <slot />
+    <V3Delete v-if="deletable" @click="deleteClickHandler" />
   </span>
 </template>
 
 <script lang="ts">
-import { computed } from 'vue';
+import { computed, SetupContext } from 'vue';
 import { size } from '../utils/sizes';
 import { color } from '../utils/colors';
+import V3Delete from './V3Delete.vue';
 
 export default {
   name: 'V3Tag',
+  components: { V3Delete },
   props: {
     size,
     color,
@@ -18,9 +21,21 @@ export default {
       type: Boolean,
       default: false,
     },
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+    deletable: {
+      type: Boolean,
+      default: false,
+    }
   },
-  setup(props: any) {
+  setup(props: any, { emit }: SetupContext) {
+
+    const deleteClickHandler = (e: Event) => emit('click-delete', e);
+
     const innerClass = computed(() => {
+      if (props.isDelete) return 'is-delete';
       const classes = [];
       if (props.color) classes.push(`is-${props.color}`);
       if (props.size) classes.push(`is-${props.size}`);
@@ -30,6 +45,7 @@ export default {
 
     return {
       innerClass,
+      deleteClickHandler,
     }
   }
 }
