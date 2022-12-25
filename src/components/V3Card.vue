c22efa00abd897b6c235f446f8b743e3a67f1b74
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-25 16:12:17 -0600

    feat: 🎸 Creating component for cards

---
+++ src/components/V3Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <div v-if="slots['card-image']" class="card-image">
+      <slot name="card-image" />
+    </div>
+    <div v-if="slots.default" class="card-content">
+      <slot name="default" />
+    </div>
+    <header v-if="slots['card-header']" class="card-header">
+      <slot name="card-header" />
+    </header>
+    <footer v-if="slots['card-footer']" class="card-footer">
+      <slot name="card-footer" />
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { SetupContext } from 'vue';
+export default {
+  name: 'V3Card',
+  setup(props: any, {slots}: SetupContext) {
+    return {
+      slots,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
