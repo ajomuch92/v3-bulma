82d23a2ce90b15a69f294a86b608bc78addff65a
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-25 17:00:04 -0600

    feat: 🎸 Creating file for installation

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import V3Box from './V3Box.vue';
+import V3Button from './V3Button.vue';
+import V3Breadcrumb from './V3Breadcrumb.vue';
+import V3Card from './V3Card.vue';
+import V3Delete from './V3Delete.vue';
+import V3Image from './V3Image.vue';
+import V3Notification from './V3Notification.vue';
+import V3Progress from './V3Progress.vue';
+import V3Tag from './V3Tag.vue';
+import V3Tags from './V3Tags.vue';
+
+export default {
+  install: (app) => {
+    app.component('V3Box', V3Box);
+    app.component('V3Button', V3Button);
+    app.component('V3Breadcrumb', V3Breadcrumb);
+    app.component('V3Card', V3Card);
+    app.component('V3Delete', V3Delete);
+    app.component('V3Image', V3Image);
+    app.component('V3Notification', V3Notification);
+    app.component('V3Progress', V3Progress);
+    app.component('V3Tag', V3Tag);
+    app.component('V3Tags', V3Tags);
+  },
+};
