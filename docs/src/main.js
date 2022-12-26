c8762aff18aa4b7172606138221dc23c417275d8
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-25 19:17:58 -0600

    refactor: 💡 Updating installation process

--- docs/src/main.js
+++ docs/src/main.js
@@ -4,9 +4,5 @@ import 'bulma/css/bulma.css';
 import V3Bulma from 'v3-bulma';
 import App from './App.vue'
 
-const app = createApp(App);
+createApp(App).use(V3Bulma).mount('#app');
 
-app.use(V3Bulma);
-
-
-app.mount('#app');
